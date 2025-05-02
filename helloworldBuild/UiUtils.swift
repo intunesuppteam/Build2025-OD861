f909aba6cf20f4d856744bbc30142425d8ee59c0
Raheem <rchisman@microsoft.com>
2025-05-02 16:42:48 -0700

    UiUtils Updated.

---
+++ helloworldBuild/UiUtils.swift
@@ -0,0 +1,32 @@
+//
+//  UiUtils.swift
+//  loginandEnrollAccount
+//
+//  Created by Raheem Chisman on 5/02/25.
+//
+
+import UIKit
+
+ 
+class UIUtils{
+    class func getCurrentViewController() -> UIViewController{
+    var topController =
+    UIApplication.shared.keyWindow?.rootViewController
+        if (nil != topController) {
+            var presentedViewController = topController!.presentedViewController
+//Loop until there are no more view controllers to go to
+while (nil != presentedViewController){topController = presentedViewController
+
+presentedViewController = topController!.presentedViewController
+
+}
+
+ }
+
+ //Return the final view controller
+
+ return topController!
+
+}
+
+   }
